041583db2d9c00a58556165a1aa6eeec7dc86f1c
Alexandr Fedorov <fedorov2602@mail.ru>
2017-10-14 13:11:19 +0300

    Added main menu

--- app/src/main/java/ru/spbau/intermessage/MainActivity.java
+++ app/src/main/java/ru/spbau/intermessage/MainActivity.java
@@ -2,6 +2,13 @@ package ru.spbau.intermessage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ListView mainMenuList = (ListView) findViewById(R.id.mainMenu);
+        final ArrayList<String> buttonNames = new ArrayList<>();
+        String name = getString(R.string.menu_dialogs);
+        buttonNames.add(name);
+        name = getString(R.string.menu_options);
+        buttonNames.add(name);
+        final ArrayAdapter mainMenuAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked, buttonNames);
+        mainMenuList.setAdapter(mainMenuAdapter);
+        mainMenuList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "It's a crutch", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
