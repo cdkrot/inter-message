6e0f31ec4e328962c3ac962d127181c30fbd99c1
Dmitry Sayutin <cdkrot@yandex.ru>
2017-12-09 21:00:17 +0300

    add kv-storage interface

--- app/src/main/java/ru/spbau/intermessage/store/IStorage.java
+++ app/src/main/java/ru/spbau/intermessage/store/IStorage.java
@@ -1,5 +1,32 @@
 package ru.spbau.intermessage.store;
 
-public interface IStorage {
+import java.util.List;
 
+public interface IStorage {
+    public enum ObjectType {
+        NULL(0),
+        STRING(1),
+        BYTE_ARRAY(2),
+        INTEGER(3);
+
+        public final int id;
+        private ObjectType(int theId) {
+            id = theId;
+        }
+    };
+    
+    public interface IObject {
+        public ObjectType getType();
+        
+        public String getString();
+        public byte[] getData();
+        public int getInt();
+
+        public void setString(String str);
+        public void setData(byte[] data);
+        public void setInt(int nt);
+    };
+
+    public IObject get(String key);
+    public List<String> getMatching(String group);
 }
