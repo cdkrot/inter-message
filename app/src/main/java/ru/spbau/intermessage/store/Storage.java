59d2597b500a35676aec0e489d50f12fc82bf0f7
Alexandr Fedorov <fedorov2602@mail.ru>
2017-12-09 20:21:09 +0300

    Changed structure of Storage

--- app/src/main/java/ru/spbau/intermessage/store/Storage.java
+++ app/src/main/java/ru/spbau/intermessage/store/Storage.java
@@ -4,23 +4,24 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class Storage extends SQLiteOpenHelper {
+public class Storage {
+    public class keyValueStore extends SQLiteOpenHelper{
+        private final String tableName = "keyValueStore";
+        private final String CREATE_TABLE = "";
+        private SQLiteDatabase db;
 
-    private final String tableName = "keyValueStore";
-    private final String CREATE_TABLE = "";
-    private SQLiteDatabase db;
+        public keyValueStore(Context context) {
+            super(context, "storage", null, 1);
+        }
 
-    public Storage(Context context) {
-        super(context, "storage", null, 1);
-    }
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+            db.execSQL(CREATE_TABLE);
+        }
 
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        db.execSQL(CREATE_TABLE);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
             db.execSQL("DROP TABLE IF EXISTS " + tableName);
+        }
     }
 }
