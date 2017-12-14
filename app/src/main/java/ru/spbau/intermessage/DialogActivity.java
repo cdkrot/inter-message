9cff47f058ecc25402fb05b2c3ca2bc9e9f49e99
Alexandr Fedorov <fedorov2602@mail.ru>
2017-12-14 19:58:03 +0300

    Realized navigation drawer

--- app/src/main/java/ru/spbau/intermessage/DialogActivity.java
+++ app/src/main/java/ru/spbau/intermessage/DialogActivity.java
@@ -4,10 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -20,20 +25,36 @@ import java.util.Objects;
 import ru.spbau.intermessage.gui.Message;
 import ru.spbau.intermessage.gui.MessageAdapter;
 
-public class DialogActivity extends AppCompatActivity {
+public class DialogActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     static final private List<Message> messages = new ArrayList<>();
     //static final private String dialogID
     private MessageReceiver messageReceiver;
     private MessageAdapter messagesAdapter;
 
-    //private DrawerLayout drawerLayout;
-    //private ListView drawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
+
+        //drawer block begins
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //drawer block ends
+
         final ListView messagesList = (ListView)findViewById(R.id.messagesList);
         final EditText input = (EditText)findViewById(R.id.input);
         messagesAdapter = new MessageAdapter(this, messages);
@@ -61,6 +82,41 @@ public class DialogActivity extends AppCompatActivity {
                 return handled;
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -100,6 +156,4 @@ public class DialogActivity extends AppCompatActivity {
             }
         }
     }
-
-
 }
