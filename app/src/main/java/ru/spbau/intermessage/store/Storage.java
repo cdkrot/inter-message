651083b2631e95b9f5ce43e3f093e02afba13cfe
Alexandr Fedorov <fedorov2602@mail.ru>
2017-12-19 23:37:21 +0300

    Implemented ListUnion and UnionList

--- app/src/main/java/ru/spbau/intermessage/store/Storage.java
+++ app/src/main/java/ru/spbau/intermessage/store/Storage.java
@@ -213,38 +213,215 @@ public class Storage implements IStorage {
             db.execSQL("DROP TABLE IF EXISTS " + tableName);
             onCreate(db);
         }
+
+        private void delete() {
+            db.execSQL("DROP TABLE IF EXISTS " + tableName);
+        }
     }
 
     private class UnionList implements IList {
 
+        final private ListStore store;
+
+        UnionList(String key) {
+            store = new ListStore(Intermessage.getAppContext(), key);
+        }
+
         @Override
         public int size() {
-            return 0;
+            try (SQLiteDatabase sqldb = store.getReadableDatabase();
+                 Cursor c = sqldb.rawQuery("select COALESCE(MAX(id)+1, 0) from " + store.tableName, null)) {
+                c.moveToFirst();
+                return c.getInt(0);
+            }
         }
 
         @Override
-        public Union get(int i) {
-            return null;
+        public Union get(int key) {
+            try (SQLiteDatabase sqldb = store.getReadableDatabase();
+                 Cursor c = sqldb.rawQuery("select * from " + store.tableName + " where id = " + key, null)) {
+                ListUnion result = new ListUnion(key);
+                if (c != null && c.moveToFirst()){
+                    if (!c.isNull(1))
+                        result.string = c.getString(1);
+                    else if (!c.isNull(2))
+                        result.v = c.getInt(2);
+                    else if (!c.isNull(3))
+                        result.data = c.getBlob(3);
+                    return result;
+                } else
+                    return result;
+            }
+        }
+
+        /**
+         * Returns Unions with ids from key to key + cnt - 1
+         */
+        @Override
+        public Union[] getBatch(int key, int cnt) {
+            List<Union> result = new ArrayList<>();
+
+            try (SQLiteDatabase sqldb = store.getReadableDatabase();
+                 Cursor c = sqldb.rawQuery("select * from " + store.tableName + " where id BETWEEN " + key + " AND " + (key + cnt - 1), null)) {
+                if (c != null && c.moveToFirst()){
+                    do {
+                        ListUnion union = new ListUnion(key);
+                        if (!c.isNull(1))
+                            union.string = c.getString(1);
+                        else if (!c.isNull(2))
+                            union.v = c.getInt(2);
+                        else if (!c.isNull(3))
+                            union.data = c.getBlob(3);
+                        result.add(union);
+                    } while (c.moveToNext());
+                } else
+                    return new Union[0];
+            }
+
+            return (Union[]) result.toArray();
         }
 
         @Override
-        public Union[] getBatch(int i, int cnt) {
-            return new Union[0];
+        public void push(int value) {
+            add(value, null, null);
         }
 
         @Override
-        public void push(Union obj) {
-
+        public void push(String value) {
+            add(null, value, null);
         }
 
         @Override
-        public void create() {
+        public void push(byte[] value) {
+            add(null, null, value);
+        }
 
+        private void add(Integer v, String s, byte[] b) {
+            ContentValues cv = new ContentValues();
+            cv.put("data", b);
+            cv.put("string", s);
+            cv.put("number", v);
+            try (SQLiteDatabase sqldb = store.getWritableDatabase()) {
+                sqldb.insert(store.tableName, null, cv);
+            }
         }
 
         @Override
         public void delete() {
+            store.delete();
+        }
 
+        private class ListUnion implements Union {
+
+            int key;
+            String string = null;
+            byte[] data = null;
+            Integer v = null;
+
+
+            ListUnion(int k) {
+                key = k;
+            }
+
+            @Override
+            public ObjectType getType() {
+                if (string != null)
+                    return ObjectType.STRING;
+                if (data != null)
+                    return ObjectType.BYTE_ARRAY;
+                if (v != null)
+                    return ObjectType.INTEGER;
+                return ObjectType.NULL;
+            }
+
+            @Override
+            public String getString() {
+                return string;
+            }
+
+            @Override
+            public byte[] getData() {
+                return data;
+            }
+
+            @Override
+            public int getInt() {
+                return v;
+            }
+
+            @Override
+            public void setString(String str) {
+                boolean was = getType() != ObjectType.NULL;
+                clear();
+                string = str;
+                if (was)
+                    update();
+                else
+                    add();
+            }
+
+            @Override
+            public void setData(byte[] d) {
+                boolean was = getType() != ObjectType.NULL;
+                clear();
+                data = d;
+                if (was)
+                    update();
+                else
+                    add();
+            }
+
+            @Override
+            public void setInt(int nt) {
+                boolean was = getType() != ObjectType.NULL;
+                clear();
+                v = nt;
+                if (was)
+                    update();
+                else
+                    add();
+            }
+
+            @Override
+            public void setNull() {
+                throw new UnsupportedOperationException();
+                /*if (getType() != ObjectType.NULL) {
+                    clear();
+                    try (SQLiteDatabase sqldb = store.getWritableDatabase()) {
+                        sqldb.delete(store.tableName, "id like '" + key + "'", null);
+                    }
+                }*/
+            }
+
+            private void clear() {
+                string = null;
+                v = null;
+                data = null;
+            }
+
+            private void update() {
+                try (SQLiteDatabase sqldb = store.getWritableDatabase()) {
+                    ContentValues cv = new ContentValues();
+                    cv.put("string", string);
+                    cv.put("data", data);
+                    cv.put("number", v);
+
+                    sqldb.update(store.tableName, cv, "id = " + key, null);
+                }
+            }
+
+            private void add() {
+                throw new UnsupportedOperationException();
+                /*try (SQLiteDatabase sqldb = store.getWritableDatabase()) {
+                    ContentValues cv = new ContentValues();
+                    cv.put("id", key);
+                    cv.put("string", string);
+                    cv.put("data", data);
+                    cv.put("number", v);
+
+                    sqldb.insert(store.tableName, null, cv);
+                }*/
+            }
         }
     }
 }
