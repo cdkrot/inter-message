8026d4f1f4906e767e0fc1a0a9c40bd1cbf8a306
Alexandr Fedorov <fedorov2602@mail.ru>
2017-12-20 23:18:09 +0300

    SharedPreferences for name of user

--- app/src/main/java/ru/spbau/intermessage/MainActivity.java
+++ app/src/main/java/ru/spbau/intermessage/MainActivity.java
@@ -1,11 +1,15 @@
 package ru.spbau.intermessage;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -13,17 +17,56 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String PREF_FILE = "preferences";
+    private final String PREF_NAME = "userName";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ListView mainMenuList = (ListView) findViewById(R.id.mainMenu);
-        final ArrayList<String> buttonNames = new ArrayList<>();
+
+        ListView mainMenuList = (ListView) findViewById(R.id.mainMenu);
+        ArrayList<String> buttonNames = new ArrayList<>();
         String name = getString(R.string.menu_dialogs);
         buttonNames.add(name);
         name = getString(R.string.menu_options);
         buttonNames.add(name);
-        final ArrayAdapter mainMenuAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, buttonNames);
+        ArrayAdapter mainMenuAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, buttonNames);
+
+        final SharedPreferences sharedPreferences = getSharedPreferences(PREF_FILE, MODE_PRIVATE);
+        String userName = sharedPreferences.getString(PREF_NAME, "");
+
+        if (userName.length() == 0) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Hello!");
+            alert.setMessage("Enter your name:");
+
+            final EditText input = new EditText(this);
+            alert.setView(input);
+
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //Nothing to do
+                }
+            });
+
+            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    String enteredName = input.getText().toString();
+                    if (enteredName.length() != 0) {
+                        SharedPreferences.Editor ed = sharedPreferences.edit();
+                        ed.putString(PREF_NAME, enteredName);
+                        ed.apply();
+                        //Controller.changeName();
+                    }
+                }
+            });
+
+            alert.show();
+        }
+
         mainMenuList.setAdapter(mainMenuAdapter);
         mainMenuList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -31,11 +74,9 @@ public class MainActivity extends AppCompatActivity {
                 if (l == 0) {
                     Intent intent = new Intent(MainActivity.this, DialogsListActivity.class);
                     startActivity(intent);
-                    //Toast.makeText(MainActivity.this, "Dialogs", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(MainActivity.this, "It is point of no return.", Toast.LENGTH_SHORT).show();
                     android.os.Process.killProcess(android.os.Process.myPid());
-
                 }
             }
         });
