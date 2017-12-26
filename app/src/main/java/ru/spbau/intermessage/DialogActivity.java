02689d927344f7aa82f91741a413b0d567d6bad0
Alexandr Fedorov <fedorov2602@mail.ru>
2017-12-26 13:41:27 +0300

    Options menu

--- app/src/main/java/ru/spbau/intermessage/DialogActivity.java
+++ app/src/main/java/ru/spbau/intermessage/DialogActivity.java
@@ -17,6 +17,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
@@ -169,6 +170,28 @@ public class DialogActivity extends AppCompatActivity
         if (messageReceiver != null) {
             unregisterReceiver(messageReceiver);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.dialog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public class MessageReceiver extends BroadcastReceiver {
