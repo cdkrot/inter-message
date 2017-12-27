b952997e9e7ea860ac321073c42a51efa380a2da
Alexandr Fedorov <fedorov2602@mail.ru>
2017-12-27 21:18:58 +0300

    Lambdas

---
+++ app/src/main/java/ru/spbau/intermessage/util/Lambda1.java
@@ -0,0 +1,8 @@
+package ru.spbau.intermessage.util;
+
+/**
+ * Created by root on 27.12.17.
+ */
+
+public interface Lambda1 {
+}
---
+++ app/src/main/java/ru/spbau/intermessage/util/Lambda2.java
@@ -0,0 +1,6 @@
+package ru.spbau.intermessage.util;
+
+
+public interface Lambda2 <A, B> {
+    public void accept(A a, B b);
+}
