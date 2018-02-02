4a8e4aca27053ac63e6cd8141a54c534fc4c605b
Alexandr Fedorov <fedorov2602@mail.ru>
2018-02-02 16:53:32 +0300

    Base of decryption

--- app/src/main/java/ru/spbau/intermessage/crypto/ID.java
+++ app/src/main/java/ru/spbau/intermessage/crypto/ID.java
@@ -1,34 +1,93 @@
 package ru.spbau.intermessage.crypto;
 
-import java.security.SecureRandom;
+import android.util.Log;
+
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+
+import org.apache.commons.codec.binary.Hex;
 
 import ru.spbau.intermessage.core.User;
 
 public class ID {
-    public ID(String priv, String pub) {
-        privkey = priv;
-        pubkey = pub;
+    private KeyPair keys;
+
+    /**
+     * Safe constructor. Keys will be cleaned after construction
+     * @param priv private key
+     * @param pub public key
+     */
+    public ID(char[] priv, char[] pub) {
+        try {
+            byte[] publicBytes = Hex.decodeHex(pub);
+            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(publicBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PublicKey publicKey = keyFactory.generatePublic(pubKeySpec);
+
+            byte[] privateBytes = Hex.decodeHex(priv);
+            PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(privateBytes);
+            keyFactory = KeyFactory.getInstance("RSA");
+            PrivateKey privateKey = keyFactory.generatePrivate(privKeySpec);
+
+            keys = new KeyPair(publicKey, privateKey);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            Arrays.fill(priv, 'a');
+            Arrays.fill(pub, 'a');
+        }
     }
 
-    private String privkey;
-    private String pubkey;
+    /**
+     * Unsafe constructor. Use char[] instead of String.
+     * @param priv private key
+     * @param pub public key
+     */
+    public ID(String priv, String pub) {
+        this(priv.toCharArray(), pub.toCharArray());
+    }
+
+    public ID(KeyPair pair) {
+        keys = pair;
+    }
 
     public static ID create() {
-        SecureRandom secureRandom = new SecureRandom();
-        String publicKey = Long.toHexString(Double.doubleToLongBits(secureRandom.nextDouble()));
-        String privateKey = Long.toHexString(Double.doubleToLongBits(secureRandom.nextDouble()));
-        return new ID(privateKey, publicKey);
+        try {
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            KeyPair pair = generator.generateKeyPair();
+            return new ID(pair);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     public String priv() {
-        return privkey;
+        return new String(Hex.encodeHex(keys.getPrivate().getEncoded()));
     }
 
     public String pub() {
-        return pubkey;
+        return new String(Hex.encodeHex(keys.getPublic().getEncoded()));
+    }
+
+    public PublicKey getPublicKey() {
+        return keys.getPublic();
+    }
+
+    public PrivateKey getPrivateKey() {
+        return keys.getPrivate();
     }
 
     public User user() {
-        return new User(pubkey);
+        return new User(pub());
     }
-}
+};
